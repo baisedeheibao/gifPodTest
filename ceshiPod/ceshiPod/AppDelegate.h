1fbda9b02ed9d3e37aaadb3465d63c59f058ab74
白色的黑豹 <>
2018-03-15 16:08:47 +0800

    123

---
+++ ceshiPod/ceshiPod/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ceshiPod
+//
+//  Created by 白色的黑豹 on 2018/3/15.
+//  Copyright © 2018年 白色的黑豹. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
